99aa98c39ead14d633017fb075f2f84c5a4b1466
valentin-vinel <valentinvnl84@gmail.com>
2025-08-20 15:46:00 +0200

    Directive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,18 @@
 import { Component, computed, signal } from '@angular/core';
 import { POKEMON_LIST } from './pokemon-list.fake';
 import { Pokemon } from './pokemon.model';
+import { PokemonBorder } from "./pokemon-border";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [PokemonBorder],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   
   pokemonList = signal(POKEMON_LIST);
-  
+
   size(pokemon: Pokemon) {
     if(pokemon.life <= 15){
       return 'Petit';
